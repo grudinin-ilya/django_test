2d82f9a1e8cbdaaf245661569566817529c86a43
ilya <grudinin_ilya@gmail.com>
2020-06-10 23:35:55 +0300

    django_testsite
    Signed-off-by: ilya <grudinin_ilya@gmail.com>

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-class News (models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True) # blank = True не обящательно к заполнению
-    created_at = models.DateTimeField(auto_now_add=True) # дата создания контента не будет изменяться
-    updated_at = models.DateTimeField(auto_now=True) # дата будет изменяться
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True) # контент по умолчанию публикуется
+
+class News(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование')
+    content = models.TextField(blank=True, verbose_name='Контент')  # blank = True не обящательно к заполнению
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Дата публикации')  # дата создания контента не будет изменяться
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')  # дата будет изменяться
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')  # контент по умолчанию публикуется
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
