274623a7301dd27f71be84256d87b519faf21e24
ilya <grudinin_ilya@gmail.com>
2020-06-20 14:34:24 +0300

    django_testsite
    Signed-off-by: ilya <grudinin_ilya@gmail.com>

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -10,7 +10,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')  # дата будет изменяться
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')  # контент по умолчанию публикуется
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"news_id": self.pk})
